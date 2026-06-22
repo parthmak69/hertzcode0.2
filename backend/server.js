604c9bd8be3ebfd7f89ce443660da1f885a86e50
faizan-shaikh-dev <faizangulab4276@gmail.com>
2026-06-22 13:37:22 +0530

    fix big in server file

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,21 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+
+dotenv.config();
 
 const app = express();
+
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-// Skeleton route for health check
-app.get('/health', (req, res) => {
-  res.json({ status: 'ok', message: 'Backend initialized successfully!' });
+app.get("/", async (req, res) => {
+  res.send("Hertzcode server is running");
 });
 
 app.listen(PORT, () => {
-  console.log(`Skeleton server is running on port ${PORT}`);
+  console.log(`http://localhost:${PORT}`);
 });
